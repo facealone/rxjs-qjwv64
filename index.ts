47ebb7f30eba6903fc33078d8aa59d7267f8d475
facealone <yanghaoyuan0958@hotmail.com>
2021-01-17 21:46:29 +0800

    1 file modified

--- index.ts
+++ index.ts
@@ -1,9 +1,41 @@
-import { of } from 'rxjs'; 
-import { map } from 'rxjs/operators';
-
+import { of, timer, interval, BehaviorSubject, ReplaySubject, Subject } from 'rxjs'; 
+import { map, scan, takeUntil, mergeMap, 
+share, shareReplay, filter, concatMap, withLatestFrom, retry, retryWhen, repeatWhen, race, reduce, repeat, refCount
+, publishLast, pairwise, pluck, publishBehavior, publishReplay, partition, publish, audit, skip,
+catchError, combineAll, combineLatest, concat, concatAll,
+count, auditTime, debounce, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 const source = of('World').pipe(
   map(x => `Hello ${x}!`)
 );
 
 source.subscribe(console.log);
+
+const myObserver = {
+  next: value => console.log('Next:', value),
+  error: err => console.log('Error', err),
+  complete: () => console.log('Complete')
+}
+
+function producer(subscribe) {
+  setTimeout(() => {
+    subscribe.next(1);
+  }, 1000);
+  subscribe.next(2);
+  setTimeout(() => {
+    subscribe.next(3);
+    subscribe.complete();
+  }, 2000);
+}
+producer(myObserver);
+
+const score$ = of(5, -10, 20, 20, 20, -5, 10);
+score$.pipe(
+  scan((acc, score) => acc + score, 0)
+).subscribe(x => console.log(x));
+
+const source$ = of(1, 2);
+const inner$ = of('A', 'B', 'C');
+source$.pipe(
+  mergeMap(() => inner$)
+).subscribe(x => console.log(x))
